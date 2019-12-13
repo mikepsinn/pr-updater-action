4fa2a047379419f668f8ccc2e61a1b9ecf7bfb92
Mike P. Sinn <m@quantimodo.com>
2019-12-13 15:47:43 -0600

    Logging

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,18 @@ const token = core.getInput('token')
 const client = new github.GitHub(token)
 
 async function main() {
+    var baseBranch = 'develop'
+    console.log(`Merging ${baseBranch} into branches with open pull requests`)
     const pullsResponse = await client.pulls.list({
         ...github.context.repo,
-        base: 'develop',
+        base: baseBranch,
         state: 'open',
     })
     const prs = pullsResponse.data
+    console.log(`Got ${prs.length} branches to update`)
+    prs.forEach(function (pr) {
+        console.log(pr)
+    })
     await Promise.all(
         prs.map((pr) => {
             client.pulls.updateBranch({
